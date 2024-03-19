cc54fd958bf82b3d13ac8f6b59c6de11a25afa66
gabriel barros <gabrielbalestrieri@hotmail.com>
2024-03-19 17:23:42 -0300

    menu criado

--- bancodigital/src/main/java/br/com/bancodigital/main.java
+++ bancodigital/src/main/java/br/com/bancodigital/main.java
@@ -1,12 +1,16 @@
 package br.com.bancodigital;
 
+import view.CartaoDeDebito;
 import view.aberturaDeConta;
 
 public class main {
   public static void main(String[] args) {
 	  aberturaDeConta conta = new aberturaDeConta();
+	  CartaoDeDebito cDebito = new CartaoDeDebito(0, conta);
 	  
 	  conta.cadastrarClienteEConta();
+	  
+	  cDebito.getSaldo();
     
   }
 }
